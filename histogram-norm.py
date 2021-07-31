9b54f3ec8422f5c7cdf91d96c04c7617e487e890
Helena <41294727+helenass97@users.noreply.github.com>
2021-07-31 23:31:18 +0100

    Update histogram-norm.py

--- histogram-norm.py
+++ histogram-norm.py
@@ -11,13 +11,14 @@ from os import path
 import torch 
 import torchvision 
 
-# # =============================================================================
-# # USING ALL DATA TO TRAIN HIST NORM - uncomment so save the images in folder
-# # =============================================================================
+# =============================================================================
+# USING ALL DATA TO TRAIN HIST NORM - uncomment so save the images in folder
+# =============================================================================
 
 # #FOR CONTROLS
 
-# dir_images="C:/Users/helen/Desktop/dataset_PET-MR/Nonlin-affine-MNI_1mm/controls/t1/"
+# #FOR T1 
+# dir_images="E:/6.Helena/Affine_reg_MNI_1mm/controls/t1/"
 # img_dir = os.listdir(dir_images)
 # dir_list=[]
 
@@ -34,11 +35,11 @@ import torchvision
 #     affine = img.affine # what is the affine in the image?? why is it needed? It is saved as nib image affine ?
 #     img = np.asanyarray(img.dataobj)
 #     img_norm = do_hist_normalization(img, perc, standard_scale) 
-#     img_norm = img_norm/100
-#     nib.save(nib.Nifti1Image(img_norm, affine), os.path.join('C:/Users/helen/Desktop/norm_data_1mm/controls/t1/', filename)) 
+#     img_norm = img_norm / 100
+#     nib.save(nib.Nifti1Image(img_norm, affine), os.path.join('E:/6.Helena/Norm_data_aff_1mm/controls/t1/', filename)) 
 
 # #FOR PET 
-# dir_images="C:/Users/helen/Desktop/dataset_PET-MR/Nonlin-affine-MNI_1mm/controls/pet/"
+# dir_images="E:/6.Helena/Affine_reg_MNI_1mm/controls/pet/"
 # img_dir = os.listdir(dir_images)
 # dir_list=[]
 
@@ -54,19 +55,19 @@ import torchvision
 #     affine = img.affine
 #     img = np.asanyarray(img.dataobj)
 #     img_norm = do_hist_normalization(img, perc, standard_scale) 
-#     img_norm = img_norm/100
-#     nib.save(nib.Nifti1Image(img_norm, affine), os.path.join('C:/Users/helen/Desktop/norm_data_1mm/controls/pet/', filename))
+#     img_norm = img_norm / 100
+#     nib.save(nib.Nifti1Image(img_norm, affine), os.path.join('E:/6.Helena/Norm_data_aff_1mm/controls/pet/', filename))
 
 
 
-# #FOR PATIENTS - might need to normalise it in respect to the controls 
+# #FOR PATIENTS - need to normalise it in respect to the controls 
 
-# dir_images="C:/Users/helen/Desktop/dataset_PET-MR/Nonlin-affine-MNI_1mm/controls/t1/"
+# dir_images="E:/6.Helena/Affine_reg_MNI_1mm/controls/t1/"
 # img_dir = os.listdir(dir_images)
 # dir_list=[]
 
 # #patients data to normalize
-# dir_images_save="C:/Users/helen/Desktop/dataset_PET-MR/Nonlin-affine-MNI_1mm/patients/t1/"
+# dir_images_save="E:/6.Helena/Affine_reg_MNI_1mm/patients/t1/"
 
 # for i in range(len(img_dir)):
 #     im = dir_images + str(img_dir[i])
@@ -81,16 +82,16 @@ import torchvision
 #     affine = img.affine # what is the affine in the image?? why is it needed? It is saved as nib image affine ?
 #     img = np.asanyarray(img.dataobj)
 #     img_norm = do_hist_normalization(img, perc, standard_scale) 
-#     img_norm = img_norm/100
-#     nib.save(nib.Nifti1Image(img_norm, affine), os.path.join('C:/Users/helen/Desktop/norm_data_1mm/patients/t1/', filename)) 
+#     img_norm = img_norm / 100
+#     nib.save(nib.Nifti1Image(img_norm, affine), os.path.join('E:/6.Helena/Norm_data_aff_1mm/patients/t1/', filename)) 
 
 # # FOR PET 
-# dir_images="C:/Users/helen/Desktop/dataset_PET-MR/Nonlin-affine-MNI_1mm/controls/pet/"
+# dir_images="E:/6.Helena/Affine_reg_MNI_1mm/controls/pet/"
 # img_dir = os.listdir(dir_images)
 # dir_list=[]
 
 # #patients data to normalize
-# dir_images_save="C:/Users/helen/Desktop/dataset_PET-MR/Nonlin-affine-MNI_1mm/patients/pet/"
+# dir_images_save="E:/6.Helena/Affine_reg_MNI_1mm/patients/pet/"
 
 # for i in range(len(img_dir)):
 #     im = dir_images + str(img_dir[i])
@@ -104,8 +105,8 @@ import torchvision
 #     affine = img.affine
 #     img = np.asanyarray(img.dataobj)
 #     img_norm = do_hist_normalization(img, perc, standard_scale) 
-#     img_norm = img_norm/100
-#     nib.save(nib.Nifti1Image(img_norm, affine), os.path.join('C:/Users/helen/Desktop/norm_data_1mm/patients/pet/', filename))
+#     img_norm = img_norm / 100
+#     nib.save(nib.Nifti1Image(img_norm, affine), os.path.join('E:/6.Helena/Norm_data_aff_1mm/patients/pet/', filename))
 
 
 #### Dont need:
@@ -150,126 +151,126 @@ import torchvision
 # nib.save(nib.Nifti1Image(average_im_t1, affine_t1), os.path.join('C:/Users/helen/Desktop/dataset_norm/patients/avg_t1/', 'average_t1_ref'))
 
 
-#plot figures and histograms 
+## plot figures and histograms 
 
-# #load images:
-# im_norm_pet_p = nib.load('C:/Users/helen/Desktop/norm_data_1mm/patients/pet/mMR_BR1_002_MNI_nonlin.nii.gz')
-# im_norm_t1_p = nib.load('C:/Users/helen/Desktop/norm_data_1mm/patients/t1/mMR_BR1_002_MNI_nonlin_1mm.nii.gz')
+#load images:
+im_norm_pet_p = nib.load('E:/6.Helena/Norm_data_aff_1mm/patients/pet/mMR_BR1_067_brain_aff_1mm.nii.gz')
+im_norm_t1_p = nib.load('E:/6.Helena/Norm_data_aff_1mm/patients/t1/mMR_BR1_067_brain_aff_1mm.nii.gz')
 
-# im_norm_t1_c = nib.load('C:/Users/helen/Desktop/norm_data_1mm/controls/t1/AZAIS_MNI_nonlin_1mm.nii.gz')
-# im_norm_pet_c = nib.load('C:/Users/helen/Desktop/norm_data_1mm/controls/pet/AZAIS_MNI_nonlin.nii.gz')
+im_norm_t1_c = nib.load('E:/6.Helena/Norm_data_aff_1mm/controls/t1/AZAIS_brain_aff_1mm.nii.gz')
+im_norm_pet_c = nib.load('E:/6.Helena/Norm_data_aff_1mm/controls/pet/AZAIS_brain_aff_1mm.nii.gz')
 
-# #controls
-# im_norm_pet_c = im_norm_pet_c.get_fdata()
-# im_norm_t1_c = im_norm_t1_c.get_fdata()
+#controls
+im_norm_pet_c = im_norm_pet_c.get_fdata()
+im_norm_t1_c = im_norm_t1_c.get_fdata()
 
-# #patients
-# im_norm_pet_p = im_norm_pet_p.get_fdata()
-# im_norm_t1_p = im_norm_t1_p.get_fdata()
-
-
-# #plot images:
-# fig= plt.figure()
-# ax = fig.add_subplot(1, 2, 1)
-# plt.imshow(im_norm_pet_c[:,:,90])
-# ax.set_title('PET norm controls')
-# ax.get_xaxis().set_visible(False)
-# ax.get_yaxis().set_visible(False)
-# ax = fig.add_subplot(1, 2, 2)
-# plt.imshow(im_norm_t1_c[:,:,90])
-# ax.set_title('T1 norm controls')
-# ax.get_xaxis().set_visible(False)
-# ax.get_yaxis().set_visible(False)
-
-
-# # plot histogram before and after: 
-# fig, axs = plt.subplots(2, 1, constrained_layout=True)
-# f1 = axs[0].hist(im_norm_pet_c.flatten(), bins=64, range=(-1,2))
-# f2 = axs[1].hist(im_norm_t1_c.flatten(), bins=64, range=(-1,2))
-# axs[0].set_title('PET controls')
-# axs[1].set_title('T1 controls')
-
-
-# #plot images:
-# fig= plt.figure()
-# ax = fig.add_subplot(1, 2, 1)
-# plt.imshow(im_norm_pet_p[:,:,90])
-# ax.set_title('PET norm patients')
-# ax.get_xaxis().set_visible(False)
-# ax.get_yaxis().set_visible(False)
-# ax = fig.add_subplot(1, 2, 2)
-# plt.imshow(im_norm_t1_p[:,:,90])
-# ax.set_title('T1 norm patients')
-# ax.get_xaxis().set_visible(False)
-# ax.get_yaxis().set_visible(False)
-
-
-# # plot histogram before and after: 
-# fig, axs = plt.subplots(2, 1, constrained_layout=True)
-# f1 = axs[0].hist(im_norm_pet_p.flatten(), bins=64, range=(-1,2))
-# f2 = axs[1].hist(im_norm_t1_p.flatten(), bins=64, range=(-1,2))
-# axs[0].set_title('PET patients')
-# axs[1].set_title('T1 patients')
-
-
-
-# Try get range 0-1 - modify nyul to 0.05-0.95
-
-#FOR T1
-
-#normalized before
-im_norm_t1 = nib.load('C:/Users/helen/Desktop/norm_data_1mm/patients/t1/mMR_BR1_002_MNI_nonlin_1mm.nii.gz')
-im_norm_t1 = im_norm_t1.get_fdata()
-
-dir_images="C:/Users/helen/Desktop/dataset_PET-MR/Nonlin-affine-MNI_1mm/controls/t1/"
-img_dir = os.listdir(dir_images)
-dir_list=[]
-
-
-for i in range(len(img_dir)):
-    im = dir_images + str(img_dir[i])
-    dir_list.append(im)
-
-standard_scale, perc = nyul_train_standard_scale(dir_list)
-
-
-img = "C:/Users/helen/Desktop/dataset_PET-MR/Nonlin-affine-MNI_1mm/patients/t1/" + 'mMR_BR1_002_MNI_nonlin_1mm.nii.gz' 
-img = nib.load(img)
-affine = img.affine
-img = np.asanyarray(img.dataobj)
-img_norm = do_hist_normalization(img, perc, standard_scale) 
-img_norm = img_norm/100
-nib.save(nib.Nifti1Image(img_norm, affine), os.path.join('C:/Users/helen/Desktop/hist_norm_new_range/', 'mMR_BR1_002_t1_new_range.nii.gz'))
+#patients
+im_norm_pet_p = im_norm_pet_p.get_fdata()
+im_norm_t1_p = im_norm_t1_p.get_fdata()
 
 
 #plot images:
 fig= plt.figure()
 ax = fig.add_subplot(1, 2, 1)
-plt.imshow(img_norm[:,:,90])
-ax.set_title('T1 new norm')
+plt.imshow(im_norm_pet_c[:,:,90], cmap="gray")
+ax.set_title('PET norm controls')
 ax.get_xaxis().set_visible(False)
 ax.get_yaxis().set_visible(False)
 ax = fig.add_subplot(1, 2, 2)
-plt.imshow(im_norm_t1[:,:,90])
-ax.set_title('T1 old norm')
+plt.imshow(im_norm_t1_c[:,:,90], cmap="gray")
+ax.set_title('T1 norm controls')
 ax.get_xaxis().set_visible(False)
 ax.get_yaxis().set_visible(False)
 
 
 # plot histogram before and after: 
 fig, axs = plt.subplots(2, 1, constrained_layout=True)
-f1 = axs[0].hist(img_norm.flatten(), bins=64, range=(np.min(img_norm),np.max(img_norm)))
-f2 = axs[1].hist(im_norm_t1.flatten(), bins=64, range=(np.min(im_norm_t1),np.max(im_norm_t1)))
-axs[0].set_title('Image T1 new norm')
-axs[1].set_title('Image T1 old Norm')
+f1 = axs[0].hist(im_norm_pet_c.flatten(), bins=64, range=(0,1))
+f2 = axs[1].hist(im_norm_t1_c.flatten(), bins=64, range=(0,1))
+axs[0].set_title('PET controls')
+axs[1].set_title('T1 controls')
 
-#####
 
+#plot images:
+fig= plt.figure()
+ax = fig.add_subplot(1, 2, 1)
+plt.imshow(im_norm_pet_p[:,:,90], cmap="gray")
+ax.set_title('PET norm patients')
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+ax = fig.add_subplot(1, 2, 2)
+plt.imshow(im_norm_t1_p[:,:,90], cmap="gray")
+ax.set_title('T1 norm patients')
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+
+
+# plot histogram before and after: 
 fig, axs = plt.subplots(2, 1, constrained_layout=True)
-f1 = axs[0].hist(img_norm.flatten(), bins=64, range=(0,1))
-f2 = axs[1].hist(im_norm_t1.flatten(), bins=64, range=(0,1))
-axs[0].set_title('Hist T1 : percent range 5-95')
-axs[1].set_title('Hist T1 : percent range 0-100')
+f1 = axs[0].hist(im_norm_pet_p.flatten(), bins=64, range=(0,1))
+f2 = axs[1].hist(im_norm_t1_p.flatten(), bins=64, range=(0,1))
+axs[0].set_title('PET patients')
+axs[1].set_title('T1 patients')
+
+
+
+# # Try get range 0-1 - modify nyul to 0.05-0.95
+
+# #FOR T1
+
+# #normalized before
+# im_norm_t1 = nib.load('C:/Users/helen/Desktop/norm_data_1mm/patients/t1/mMR_BR1_002_MNI_nonlin_1mm.nii.gz')
+# im_norm_t1 = im_norm_t1.get_fdata()
+
+# dir_images="C:/Users/helen/Desktop/dataset_PET-MR/Nonlin-affine-MNI_1mm/controls/t1/"
+# img_dir = os.listdir(dir_images)
+# dir_list=[]
+
+
+# for i in range(len(img_dir)):
+#     im = dir_images + str(img_dir[i])
+#     dir_list.append(im)
+
+# standard_scale, perc = nyul_train_standard_scale(dir_list)
+
+
+# img = "C:/Users/helen/Desktop/dataset_PET-MR/Nonlin-affine-MNI_1mm/patients/t1/" + 'mMR_BR1_002_MNI_nonlin_1mm.nii.gz' 
+# img = nib.load(img)
+# affine = img.affine
+# img = np.asanyarray(img.dataobj)
+# img_norm = do_hist_normalization(img, perc, standard_scale) 
+# img_norm = img_norm/100
+# nib.save(nib.Nifti1Image(img_norm, affine), os.path.join('C:/Users/helen/Desktop/hist_norm_new_range/', 'mMR_BR1_002_t1_new_range.nii.gz'))
+
+
+# #plot images:
+# fig= plt.figure()
+# ax = fig.add_subplot(1, 2, 1)
+# plt.imshow(img_norm[:,:,90])
+# ax.set_title('T1 new norm')
+# ax.get_xaxis().set_visible(False)
+# ax.get_yaxis().set_visible(False)
+# ax = fig.add_subplot(1, 2, 2)
+# plt.imshow(im_norm_t1[:,:,90])
+# ax.set_title('T1 old norm')
+# ax.get_xaxis().set_visible(False)
+# ax.get_yaxis().set_visible(False)
+
+
+# # plot histogram before and after: 
+# fig, axs = plt.subplots(2, 1, constrained_layout=True)
+# f1 = axs[0].hist(img_norm.flatten(), bins=64, range=(np.min(img_norm),np.max(img_norm)))
+# f2 = axs[1].hist(im_norm_t1.flatten(), bins=64, range=(np.min(im_norm_t1),np.max(im_norm_t1)))
+# axs[0].set_title('Image T1 new norm')
+# axs[1].set_title('Image T1 old Norm')
+
+# #####
+
+# fig, axs = plt.subplots(2, 1, constrained_layout=True)
+# f1 = axs[0].hist(img_norm.flatten(), bins=64, range=(0,1))
+# f2 = axs[1].hist(im_norm_t1.flatten(), bins=64, range=(0,1))
+# axs[0].set_title('Hist T1 : percent range 5-95')
+# axs[1].set_title('Hist T1 : percent range 0-100')
 
 # #=============================================================================
 # # Using only one image as reference - uncomment so save the images in folder
