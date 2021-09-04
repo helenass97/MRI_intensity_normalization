07822ddc65a32f783d581c4765289ae41ac40c2a
Helena Sousa <helenagarajau@hotmail.com>
2021-09-04 16:58:14 +0100

    updated norm

--- histogram-norm.py
+++ histogram-norm.py
@@ -1,7 +1,10 @@
 
 # Histogram Normalization 
 import os
+import cv2
+
 import numpy as np
+from monai.transforms import GaussianSmooth
 import nibabel as nib
 from nyul import nyul_train_standard_scale, nyul_apply_standard_scale, do_hist_normalization
 import matplotlib.pyplot as plt
@@ -151,66 +154,71 @@ import torchvision
 # nib.save(nib.Nifti1Image(average_im_t1, affine_t1), os.path.join('C:/Users/helen/Desktop/dataset_norm/patients/avg_t1/', 'average_t1_ref'))
 
 
-## plot figures and histograms 
+# ## plot figures and histograms 
 
-#load images:
-im_norm_pet_p = nib.load('E:/6.Helena/Norm_data_aff_1mm/patients/pet/mMR_BR1_067_brain_aff_1mm.nii.gz')
-im_norm_t1_p = nib.load('E:/6.Helena/Norm_data_aff_1mm/patients/t1/mMR_BR1_067_brain_aff_1mm.nii.gz')
+# #load images:
+# im_norm_pet_p = nib.load('E:/6.Helena/Norm_data_aff_1mm/patients/pet/mMR_BR1_067_brain_aff_1mm.nii.gz')
+# im_norm_t1_p = nib.load('E:/6.Helena/Norm_data_aff_1mm/patients/t1/mMR_BR1_067_brain_aff_1mm.nii.gz')
 
-im_norm_t1_c = nib.load('E:/6.Helena/Norm_data_aff_1mm/controls/t1/AZAIS_brain_aff_1mm.nii.gz')
-im_norm_pet_c = nib.load('E:/6.Helena/Norm_data_aff_1mm/controls/pet/AZAIS_brain_aff_1mm.nii.gz')
+# im_norm_t1_c = nib.load('E:/6.Helena/Norm_data_aff_1mm/controls/t1/AZAIS_brain_aff_1mm.nii.gz')
+# im_norm_pet_c = nib.load('E:/6.Helena/Norm_data_aff_1mm/controls/pet/AZAIS_brain_aff_1mm.nii.gz')
 
-#controls
-im_norm_pet_c = im_norm_pet_c.get_fdata()
-im_norm_t1_c = im_norm_t1_c.get_fdata()
+# #controls
+# im_norm_pet_c = im_norm_pet_c.get_fdata()
+# im_norm_t1_c = im_norm_t1_c.get_fdata()
 
-#patients
-im_norm_pet_p = im_norm_pet_p.get_fdata()
-im_norm_t1_p = im_norm_t1_p.get_fdata()
-
-
-#plot images:
-fig= plt.figure()
-ax = fig.add_subplot(1, 2, 1)
-plt.imshow(im_norm_pet_c[:,:,90], cmap="gray")
-ax.set_title('PET norm controls')
-ax.get_xaxis().set_visible(False)
-ax.get_yaxis().set_visible(False)
-ax = fig.add_subplot(1, 2, 2)
-plt.imshow(im_norm_t1_c[:,:,90], cmap="gray")
-ax.set_title('T1 norm controls')
-ax.get_xaxis().set_visible(False)
-ax.get_yaxis().set_visible(False)
+# #patients
+# im_norm_pet_p = im_norm_pet_p.get_fdata()
+# im_norm_t1_p = im_norm_t1_p.get_fdata()
 
 
-# plot histogram before and after: 
-fig, axs = plt.subplots(2, 1, constrained_layout=True)
-f1 = axs[0].hist(im_norm_pet_c.flatten(), bins=64, range=(0,1))
-f2 = axs[1].hist(im_norm_t1_c.flatten(), bins=64, range=(0,1))
-axs[0].set_title('PET controls')
-axs[1].set_title('T1 controls')
+# #plot images:
+# fig= plt.figure()
+# ax = fig.add_subplot(1, 2, 1)
+# plt.imshow(im_norm_pet_c[:,:,90], cmap="gray")
+# ax.set_title('PET norm controls')
+# ax.get_xaxis().set_visible(False)
+# ax.get_yaxis().set_visible(False)
+# ax = fig.add_subplot(1, 2, 2)
+# plt.imshow(im_norm_t1_c[:,:,90], cmap="gray")
+# ax.set_title('T1 norm controls')
+# ax.get_xaxis().set_visible(False)
+# ax.get_yaxis().set_visible(False)
 
 
-#plot images:
-fig= plt.figure()
-ax = fig.add_subplot(1, 2, 1)
-plt.imshow(im_norm_pet_p[:,:,90], cmap="gray")
-ax.set_title('PET norm patients')
-ax.get_xaxis().set_visible(False)
-ax.get_yaxis().set_visible(False)
-ax = fig.add_subplot(1, 2, 2)
-plt.imshow(im_norm_t1_p[:,:,90], cmap="gray")
-ax.set_title('T1 norm patients')
-ax.get_xaxis().set_visible(False)
-ax.get_yaxis().set_visible(False)
+# # plot histogram before and after: 
+# fig, axs = plt.subplots(2, 1, constrained_layout=True)
+# f1 = axs[0].hist(im_norm_pet_c.flatten(), bins=64, range=(0,1))
+# f2 = axs[1].hist(im_norm_t1_c.flatten(), bins=64, range=(0,1))
+# axs[0].set_title('PET controls')
+# axs[1].set_title('T1 controls')
 
 
-# plot histogram before and after: 
-fig, axs = plt.subplots(2, 1, constrained_layout=True)
-f1 = axs[0].hist(im_norm_pet_p.flatten(), bins=64, range=(0,1))
-f2 = axs[1].hist(im_norm_t1_p.flatten(), bins=64, range=(0,1))
-axs[0].set_title('PET patients')
-axs[1].set_title('T1 patients')
+# #plot images:
+# fig= plt.figure()
+# ax = fig.add_subplot(1, 2, 1)
+# plt.imshow(im_norm_pet_p[:,:,90], cmap="gray")
+# ax.set_title('PET norm patients')
+# ax.get_xaxis().set_visible(False)
+# ax.get_yaxis().set_visible(False)
+# ax = fig.add_subplot(1, 2, 2)
+# plt.imshow(im_norm_t1_p[:,:,90], cmap="gray")
+# ax.set_title('T1 norm patients')
+# ax.get_xaxis().set_visible(False)
+# ax.get_yaxis().set_visible(False)
+
+
+# # plot histogram before and after: 
+# fig, axs = plt.subplots(2, 1, constrained_layout=True)
+# f1 = axs[0].hist(im_norm_pet_p.flatten(), bins=64, range=(0,1))
+# f2 = axs[1].hist(im_norm_t1_p.flatten(), bins=64, range=(0,1))
+# axs[0].set_title('PET patients')
+# axs[1].set_title('T1 patients')
+
+
+
+
+
 
 
 
@@ -374,3 +382,186 @@ axs[1].set_title('T1 patients')
 # axs[0].set_title('Image T1 Original')
 # axs[1].set_title('Image T1 Normalized')
 
+
+# =============================================================================
+#  NEW: Normalise only Patients in respect to patients - Use when training
+# =============================================================================
+
+## only with the patients data 
+
+# dir_images="E:/Master/PET-MR/PET-MR dataset/Hdata-affineMNI-1mm-brain/patients/t1/"
+# img_dir = os.listdir(dir_images)
+# dir_list=[]
+
+# #patients data to normalize
+# dir_images_save="E:/Master/PET-MR/PET-MR dataset/Hdata-affineMNI-1mm-brain/patients/t1/"
+
+# for i in range(len(img_dir)):
+#     im = dir_images + str(img_dir[i])
+#     dir_list.append(im)
+
+# standard_scale, perc = nyul_train_standard_scale(dir_list)
+
+
+# for filename in sorted(os.listdir(dir_images_save)):
+#     img = dir_images_save + filename 
+#     img = nib.load(img)
+#     affine = img.affine 
+#     img = np.asanyarray(img.dataobj)
+#     img_norm = do_hist_normalization(img, perc, standard_scale) 
+#     img_norm = img_norm / 100
+#     nib.save(nib.Nifti1Image(img_norm, affine), os.path.join('E:/Master/PET-MR/PET-MR dataset/Helena-patientsonly-histnorm/patients/t1/', filename)) 
+
+# # FOR PET 
+# dir_images="E:/Master/PET-MR/PET-MR dataset/Hdata-affineMNI-1mm-brain/patients/pet/"
+# img_dir = os.listdir(dir_images)
+# dir_list=[]
+
+# #patients data to normalize
+# dir_images_save="E:/Master/PET-MR/PET-MR dataset/Hdata-affineMNI-1mm-brain/patients/pet/"
+
+# for i in range(len(img_dir)):
+#     im = dir_images + str(img_dir[i])
+#     dir_list.append(im)
+
+# standard_scale, perc = nyul_train_standard_scale(dir_list)
+
+# for filename in sorted(os.listdir(dir_images_save)):
+#     img = dir_images_save + filename 
+#     img = nib.load(img)
+#     affine = img.affine
+#     img = np.asanyarray(img.dataobj)
+#     img_norm = do_hist_normalization(img, perc, standard_scale) 
+#     img_norm = img_norm / 100
+#     nib.save(nib.Nifti1Image(img_norm, affine), os.path.join('E:/Master/PET-MR/PET-MR dataset/Helena-patientsonly-histnorm/patients/pet/', filename))
+
+
+## plot figures and histograms 
+
+# #load images:
+# im_pet_or = nib.load('E:/Master/PET-MR/PET-MR dataset/Hdata-affineMNI-1mm-brain/patients/pet/mMR_BR1_008_brain_aff_1mm.nii.gz')     
+# im_t1_or = nib.load('E:/Master/PET-MR/PET-MR dataset/Hdata-affineMNI-1mm-brain/patients/t1/mMR_BR1_008_brain_aff_1mm.nii.gz')
+
+# im_norm_pet_p = nib.load('E:/Master/PET-MR/PET-MR dataset/Helena-patientsonly-histnorm/patients/pet/mMR_BR1_008_brain_aff_1mm.nii.gz')
+# im_norm_t1_p = nib.load('E:/Master/PET-MR/PET-MR dataset/Helena-patientsonly-histnorm/patients/t1/mMR_BR1_008_brain_aff_1mm.nii.gz')
+
+# #patients
+# im_norm_pet_p = im_norm_pet_p.get_fdata()
+# im_norm_t1_p = im_norm_t1_p.get_fdata()
+
+# im_or_pet = im_pet_or.get_fdata()
+# im_or_t1 = im_t1_or.get_fdata()
+
+# #plot images:
+# fig= plt.figure()
+# ax = fig.add_subplot(1, 2, 1)
+# plt.imshow(im_or_pet[:,:,90], cmap="gray")
+# ax.set_title('PET original')
+# ax.get_xaxis().set_visible(False)
+# ax.get_yaxis().set_visible(False)
+# ax = fig.add_subplot(1, 2, 2)
+# plt.imshow(im_or_t1[:,:,90], cmap="gray")
+# ax.set_title('T1 original')
+# ax.get_xaxis().set_visible(False)
+# ax.get_yaxis().set_visible(False)
+
+# fig= plt.figure()
+# ax = fig.add_subplot(1, 2, 1)
+# plt.imshow(im_norm_pet_p[:,:,90], cmap="gray")
+# ax.set_title('PET norm')
+# ax.get_xaxis().set_visible(False)
+# ax.get_yaxis().set_visible(False)
+# ax = fig.add_subplot(1, 2, 2)
+# plt.imshow(im_norm_t1_p[:,:,90], cmap="gray")
+# ax.set_title('T1 norm')
+# ax.get_xaxis().set_visible(False)
+# ax.get_yaxis().set_visible(False)
+
+# # plot histogram before and after: 
+# fig, axs = plt.subplots(2, 1, constrained_layout=True)
+# f1 = axs[0].hist(im_norm_pet_p.flatten(), bins=64, range=(0,1))
+# f2 = axs[1].hist(im_norm_t1_p.flatten(), bins=64, range=(0,1))
+# axs[0].set_title('PET')
+# axs[1].set_title('T1')
+
+# =============================================================================
+# Remask background - after histogram normalisation 
+# =============================================================================
+# directories for images and masks 
+
+# dir_t1_p = 'E:/Master/PET-MR/PET-MR dataset/Helena-patientsonly-histnorm/patients/t1/'
+# dir_pet_p = 'E:/Master/PET-MR/PET-MR dataset/Helena-patientsonly-histnorm/patients/pet/'
+
+# dir_mask_t1_p = 'D:/6.Helena/Norm_data_aff_1mm/masks/patients/t1/'
+# dir_mask_pet_p = 'D:/6.Helena/Norm_data_aff_1mm/masks/patients/pet/'
+
+# # #T1
+# names_t1_p = os.listdir(dir_t1_p)
+
+# # #PET
+# names_pet_p = os.listdir(dir_pet_p) 
+
+# # MASKS 
+# names_masks_p = os.listdir(dir_mask_t1_p)
+# names_masks_pet_p = os.listdir(dir_mask_pet_p)
+
+# # #function to smooth masks
+# smooth_tr = GaussianSmooth()
+
+# # Remask background T1 of patients
+# for i in range(len(names_t1_p)):
+#     name_image= names_t1_p[i]
+#     image = nib.load(dir_t1_p + name_image)
+#     affine = image.affine
+#     image = image.get_fdata()
+    
+    
+#     name_mask = names_masks_p[i]
+#     mask = nib.load(dir_mask_t1_p + name_mask)
+#     mask = mask.get_fdata()
+    
+#     #smooth mask and use that to multiply 
+#     mask = smooth_tr(mask).astype('float64')
+    
+#     image_remask = cv2.multiply(image, mask)
+#     image_r= image_remask*(image_remask>0)
+#     nib.save(nib.Nifti1Image(image_r, affine), os.path.join('E:/Master/PET-MR/PET-MR dataset/Helena-patientsonly-histnorm/remasked_patients/t1/', name_image))
+
+# # Remask background PET of patients
+# for i in range(len(names_pet_p)):
+#     name_image= names_pet_p[i]
+#     image = nib.load(dir_pet_p + name_image)
+#     affine = image.affine
+#     image = image.get_fdata()
+      
+#     name_mask = names_masks_pet_p[i]
+#     mask = nib.load(dir_mask_pet_p  + name_mask)
+#     mask = mask.get_fdata()
+    
+#     #smooth mask and use that to multiply 
+#     mask = smooth_tr(mask).astype('float64')
+    
+#     image_remask = cv2.multiply(image, mask)
+#     image_r= image_remask*(image_remask>0) # all the neagtive values inside the mask go to 0 
+#     nib.save(nib.Nifti1Image(image_r, affine), os.path.join('E:/Master/PET-MR/PET-MR dataset/Helena-patientsonly-histnorm/remasked_patients/pet/', name_image))
+
+# #load images:
+im_rem_pet_p = nib.load('E:/Master/PET-MR/PET-MR dataset/Helena-patientsonly-histnorm/remasked_patients/pet/mMR_BR1_008_brain_aff_1mm.nii.gz')
+im_rem_t1_p = nib.load('E:/Master/PET-MR/PET-MR dataset/Helena-patientsonly-histnorm/remasked_patients/t1/mMR_BR1_008_brain_aff_1mm.nii.gz')
+
+im_pet_p = im_rem_pet_p.get_fdata()
+im_t1_p = im_rem_t1_p.get_fdata()
+
+
+#plot images:
+fig= plt.figure()
+ax = fig.add_subplot(1, 2, 1)
+plt.imshow(im_pet_p[:,:,90], cmap="gray")
+ax.set_title('PET remasked')
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+ax = fig.add_subplot(1, 2, 2)
+plt.imshow(im_t1_p[:,:,90], cmap="gray")
+ax.set_title('T1 remasked')
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
